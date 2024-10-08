022d8c3c53c2381c9d0d3c50c7b717e004aed316
morgancheung914 <morgancheung914@gmail.com>
2024-10-09 03:44:05 +0800

    Dataset preprocessing!

--- main_staging.py
+++ main_staging.py
@@ -1,20 +1,52 @@
-from src.med42 import Med42
+##from src.med42 import Med42
 from src.llama3 import Llama3
+from preprocess import process_data, chat_formatter
+import yaml
+with open('config.yaml', 'r') as file:
+    configs = yaml.safe_load(file)
+
+
+
 
 # TODO: Choosing the models from the config 
 
-model = Med42(None)
+model = Llama3(None)
 
 model.load_model()
 
+bench_datasets = set(configs['dataset']['dataset_names'])
 #load dataset
 
-messages = [
-    {"role": "system", "content": "Please answer the question below"},
-    {"role": "user", "content": "Who are you?"},
-]
+datasets_dict = process_data(d = bench_datasets, cache_dir = 'dataset/cache')
+print(f">Bench>: Datasets loaded: {configs['dataset']['dataset_names']}")
+for dataset in list(datasets_dict.keys()):
+    if datasets_dict[dataset] == None:
+        continue 
+    else:
+        datasets_dict[dataset] = chat_formatter(datasets_dict[dataset], name = dataset, tokenizer = model.tokenizer)
+print(f">Bench>: Datasets preprocessing finished: {configs['dataset']['dataset_names']}")
 
-print(model.predict(messages, max_length = 1000, num_return_seq = 1, temperature = 1.5))
+print(datasets_dict['PubMedQA']['test'][0])
+
+# if "PubMedQA" in bench_datasets:
+#     PubMedQA_ds = chat_formatter(PubMedQA_ds, name = "PubMedQA", question='question', system_prompt = "Please answer this question with Yes, No or Maybe.")
+# if "MedMCQA" in bench_datasets:
+#     MedMCQA_ds = chat_formatter(MedMCQA_ds, name = "MedMCQA")
+# if "MedMCQA" in bench_datasets:
+# MedMCQA_ds = chat_formatter(MedMCQA_ds, name = "MedMCQA")
+# if "MedMCQA" in bench_datasets:
+# MedMCQA_ds = chat_formatter(MedMCQA_ds, name = "MedMCQA")
+# if "MedMCQA" in bench_datasets:
+# MedMCQA_ds = chat_formatter(MedMCQA_ds, name = "MedMCQA")
+# if "MedMCQA" in bench_datasets:
+# MedMCQA_ds = chat_formatter(MedMCQA_ds, name = "MedMCQA")
+#messages = [
+    #{"role": "system", "content": "Please answer the question below"},
+    #{"role": "user", "content": "Who are you?"},
+#]
+
+
+#print(model.predict(messages, max_length = 1000, num_return_seq = 1, temperature = 1.5))
 
 
 
--- preprocess.py
+++ preprocess.py
@@ -2,34 +2,80 @@ from datasets import load_dataset
 import argparse 
 import re 
 
-def preprocess(d, c): # takes in dataset and cache_dir configs 
-    dataset = set([i for i in re.split(',| ', d)])
+def process_data(d, cache_dir): # takes in dataset and cache_dir configs 
+    dataset = set(d)
     
     PubMedQA_ds, MedMCQA_ds, anatomy_ds, biology_ds, medicine_ds, clinical_ds = None, None, None, None, None, None
-
+    
     
     # {'pub_id', 'question', 'context', 'long_answer', 'final_decision'}
-    if 'PubMedQA' in dataset or 'all' in dataset:
-        PubMedQA_ds = load_dataset("qiaojin/PubMedQA", "pqa_labeled", split='train', cache_dir=c)
+    if 'PubMedQA' in dataset:
+        PubMedQA_ds = load_dataset("qiaojin/PubMedQA", "pqa_labeled", split='train', cache_dir=cache_dir)
         PubMedQA_ds = PubMedQA_ds.train_test_split(test_size=0.5) # train, test 
+    
+    
 
     # {'id', 'question', 'opa', 'opb', 'opc', 'opd', 'cop', 'choice_type', 'exp', 'subject_name', 'topic_name'}
-    if 'MedMCQA' in dataset or 'all' in dataset:
+    if 'MedMCQA' in dataset:
         MedMCQA_ds = load_dataset("openlifescienceai/medmcqa") # train, test, validation 
 
     # {'question', 'subject', 'choices', 'answer'}
-    if 'MMLU_anatomy' in dataset or 'all' in dataset:  
-        anatomy_ds = load_dataset("cais/mmlu", "anatomy", cache_dir=c) # test, validation 
+    if 'MMLU_anatomy' in dataset:  
+        anatomy_ds = load_dataset("cais/mmlu", "anatomy", cache_dir=cache_dir) # test, validation 
 
-    if 'MMLU_biology' in dataset or 'all' in dataset:
-        biology_ds = load_dataset("cais/mmlu", "college_biology", cache_dir=c) # test, validation 
+    if 'MMLU_biology' in dataset:
+        biology_ds = load_dataset("cais/mmlu", "college_biology", cache_dir=cache_dir) # test, validation 
 
-    if 'MMLU_medicine' in dataset or 'all' in dataset:
-        medicine_ds = load_dataset("cais/mmlu", "college_medicine", cache_dir=c) # test, validation 
+    if 'MMLU_medicine' in dataset:
+        medicine_ds = load_dataset("cais/mmlu", "college_medicine", cache_dir=cache_dir) # test, validation 
 
-    if 'MMLU_clinical' in dataset or 'all' in dataset:
-        clinical_ds = load_dataset("cais/mmlu", "clinical_knowledge", cache_dir=c) # test, validation 
+    if 'MMLU_clinical' in dataset:
+        clinical_ds = load_dataset("cais/mmlu", "clinical_knowledge", cache_dir=cache_dir) # test, validation 
+
+    ret_dict = {"PubMedQA": PubMedQA_ds, "MedMCQA_ds": MedMCQA_ds, "MMLU_anatomy": anatomy_ds, "MMLU_biology": biology_ds, "MMLU_medicine": medicine_ds, "MMLU_clinical": clinical_ds}
+    return ret_dict
 
 
-    return PubMedQA_ds, MedMCQA_ds, anatomy_ds, biology_ds, medicine_ds, clinical_ds
 
+    
+
+def format_chat_template(row, qcol, system_prompt, tokenizer):
+    row_json = [{"role": "system", "content": system_prompt},
+                {"role": "user", "content": row[qcol]}]
+    
+    return tokenizer.apply_chat_template(row_json, tokenize=False)
+
+def format_MedMcQa(row, tokenizer):
+    row_json = [{"role": "system", "content": "Please read the question and pick the most suitable choice from A to D, simply answer A, B, C or D"},
+                {"role": "user", "content": "Question: " + row["question"] + "Choices: " + f""" A: {row["opa"]}, B: {row["opb"]}, C: {row["opc"]}, D: {row["opd"]}"""},
+              ]
+    
+    return tokenizer.apply_chat_template(row_json, tokenize=False)
+
+def format_MMLU(row, tokenizer):
+
+    row_json = [{"role": "system", "content": "Please read the question and pick the most suitable choice from A to D, simply answer A, B, C or D"},
+                {"role": "user", "content": "Question: " + row['question'] + "Choices: " + f"A: {row['choices'][0]}, B: {row['choices'][1]}, C: {row['choices'][2]}, D: {row['choices'][3]}"},
+                ]
+
+    
+    return tokenizer.apply_chat_template(row_json, tokenize=False)
+
+
+def chat_formatter(dataset, name = None, question='question', system_prompt = "Please answer this question with Yes, No or Maybe.", tokenizer = None):
+    
+    if name == "MedMCQA":
+        dataset = dataset.map(
+        format_MedMcQa,
+        fn_kwargs = {"tokenizer": tokenizer},
+        num_proc=4,
+    )
+    elif name == "PubMedQA":
+        dataset = dataset.map(
+            format_chat_template,
+            fn_kwargs = {'qcol': question, 'system_prompt': system_prompt, "tokenizer": tokenizer},
+            num_proc=4,
+        )
+    else:
+        dataset = dataset.map(format_MMLU, fn_kwargs = {"tokenizer": tokenizer}, num_proc=4)
+    return dataset
