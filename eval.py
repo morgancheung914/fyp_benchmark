e5349a0580260a24bcdb2f0e475537b0700372fb
Morgan Cheung <morgancheung914@gmail.com>
2024-10-21 20:55:03 +0800

    Added main loop for Groq shortening

--- eval.py
+++ eval.py
@@ -7,27 +7,32 @@ import os
 from groq import Groq
 
 # Function to query the LLaMA3 model
-def query_llama3(text):
+def query_llama3(text, dataset_name):
 
     #get api key from environment
     client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
     
-    choices = "A, B, C or D"
-    choices = "Yes No or Maybe"
-    messages = [{"role": "system", "content": f"""
-    The user will provide you a sentence that answers a biomedical question, please determine the short answer of the sentence from this list of choices: {choices}
-    Please answer with the short answer only.
-    """},
+    if dataset_name in ["MMLU_biology", "MMLU_anatomy", "MMLU_medicine", "MMLU_clinical"]:
+        choices = "A, B, C or D"
+    if dataset_name in ["PubMedQA"]:
+        choices = "Yes No or Maybe"
+        
+    messages = [
     {
-        "role": "user", "content": text
+        "role": "user", "content": f"""This is a string that answers a biomedical question, please determine the short answer of the sentence from this list of choices: {choices}
+    Please answer with the short answer only and nothing else. This is the string: \"""" + text + "\" This is the end of the string."
     }
+    
     ]
+    
     chat_completion = client.chat.completions.create(
         messages = messages,
-        model = "llama3-8b-8192"
+        model = "llama-3.1-70b-versatile"
+        
     )
     
-    print(chat_completion.choices[0].message.content)
+    return chat_completion.choices[0].message.content
+
 
     # Make the request
     #response = requests.post(api_endpoint, json=payload, headers=headers)
@@ -36,23 +41,26 @@ def query_llama3(text):
     # Extract the response
     #return response_data['choices'][0]['text'].strip()
 
-def process_example(example):
-    example["processed_answer"] = query_llama3(example["response"])
+def process_example(example, dataset_name):
+    example["processed_answer"] = query_llama3(example["response"], dataset_name)
 
     return example 
 
-def evaluate(dataset_path, dataset_name):
+def evaluate(dataset_path, dataset_name, model_name):
     
     dataset = load_from_disk(dataset_path)
 
     # Process each response in the dataset
-    processed_dataset = dataset.map(process_example)
+    
+    processed_dataset = dataset.map(process_example, fn_kwargs={"dataset_name": dataset_name})
 
     # Save the updated dataset
-    processed_dataset.save_to_disk(dataset_path)
+    processed_dataset.to_json(f"shortened/{model_name}/{dataset_name}")
 
     print(f">Eval>: {dataset_name} answer processing finished and saved.")
 
+
+    return
 
 def main():
 
@@ -70,18 +78,18 @@ def main():
     # Load the rendered YAML
     rendered_config = yaml.safe_load(rendered_content)
 
-
+    
     
 
     d_paths = rendered_config['chosen_datasets']
-
+    print(d_paths)
   
     for d in list(d_paths.keys()):
         #get dataset name and path
-
+        print(d_paths)
         d_path = d_paths[d]
 
-        evaluate(d_path, d)
-        pass
+        evaluate(d_path, d, model_name)
+        
 
 main()
