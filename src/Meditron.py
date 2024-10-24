33abcffd32034d4b70f119338e9a7b073b84a888
wzqacky <wzqackyxd@gmail.com>
2024-10-24 16:23:54 +0800

    modified Meditron

--- src/Meditron.py
+++ src/Meditron.py
@@ -4,5 +4,10 @@ class MeditronModel(BaseModel):
     def __init__(self, config=None):
         super().__init__(config={"Autoregressive": True}, internal_id = "Meditron", model_id = "epfl-llm/meditron-7b")
 
-    def predict(self, input_text, max_length, num_return_seq, temperature):
-        pass 
+    def chat_template(self, input):
+        return f"{input[0]['content']}\n ###User: {input[1]['content']}\n ###Assistant: "
+
+    def batch_predict(self, input_text, max_length, num_return_seq, temperature):
+        inputs = self.tokenizer(self.chat_template(input_text), return_tensors="pt")
+        generate_ids = self.model.generate(inputs.input_ids, max_length=max_length)
+        return self.tokenizer.decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
