ac04dc322fc63e69613cef8c490c6dc605af0d37
wzqacky <wzqackyxd@gmail.com>
2024-12-01 22:58:09 +0800

    renamed main.py to inference.py

--- inference.py
+++ inference.py
@@ -26,27 +26,28 @@ model.load_model()
 
 # get the required datasets from config 
 bench_datasets = set(configs['dataset']['dataset_names'])
+
+# get the generation parameters 
 number_shot = configs['generation']['few_shot']
 CoT = configs['generation']['CoT']
 self_cons = configs['generation']['k_self_consistency']
+top_p = configs['generation']['top_p']
+temp = configs['generation']['temperature']
+batch_size = configs['generation']['batch_size']
 
-#Enforce CoT if self_cons is set to true
-if self_cons != False:
+# Enforce CoT if self_cons is set to true
+if self_cons:
     CoT = True
 
 # load datasets, the returned dict stores the processed datasetDict w.r.t each dataset 
-datasets_dict = process_data(bench_datasets, 'dataset/cache', number_shot, CoT)
+datasets_dict = process_data(bench_datasets, cache_dir='dataset/cache', number_shot=number_shot, CoT=CoT)
 print(f">Bench>: Datasets loaded: {configs['dataset']['dataset_names']}")
 
 
 # inference 
 for dataset in list(datasets_dict.keys()):
-    if datasets_dict[dataset] == None:
-        if dataset in bench_datasets:
-            print(f">Error>: Error encountered when processing {dataset}")
-
-        else:
-            continue 
+    if dataset in bench_datasets and datasets_dict[dataset] == None:
+        print(f">Error>: Error encountered when processing {dataset}")
 
     else:
         # parse the contents into the designated prompt template 
@@ -71,35 +72,38 @@ for dataset in list(datasets_dict.keys()):
                 
                 {"role": "user", "content": i['user_content']}] for i in selected_ds]
 
-        dataloader = DataLoader(ds_test, batch_size = 1, shuffle=False, collate_fn = lambda x: x)
+        dataloader = DataLoader(ds_test, batch_size = batch_size, shuffle=False, collate_fn = lambda x: x)
         responses = []
         print(f">Bench>: Starting inference on {dataset}.")
 
         for batch in tqdm(dataloader):
             #print(batch)
-            num_seq = 1 if self_cons == False else self_cons #set number of sequences to generate
-            print(num_seq)
-            batch_responses = (model.batch_predict(batch, max_length = 300, num_return_seq = num_seq, temperature = 1, top_p = 0.9))
-            #print(batch_responses)
-            print(f"batch: {batch_responses}\n")
-            print(f"len: {len(batch_responses)}")
+            num_seq = 1 if not self_cons else self_cons #set number of sequences to generate
+  
+            batch_responses = (model.batch_predict(batch, max_length = 300, num_return_seq = num_seq, temperature = temp, top_p = top_p))
 
-            if self_cons != False: # if self_consistency is in effect, divide the list of results with the # of self-cons generations
+            #print(f"batch: {batch_responses}\n")
+            #print(f"len: {len(batch_responses)}")
+
+            if self_cons: # if self_consistency is in effect, divide the list of results with the # of self-cons generations
                 for i in range(1):
                     curr = batch_responses[i*self_cons: i*self_cons + self_cons]
                     #pack as json string
-                    curr_json =json.dumps(curr)
+                    curr_json = json.dumps(curr)
                     responses.append(curr_json)
-                    print(responses)
+                    #print(responses)
 
             else:
                 #self_consistency is not in effect
                 responses.extend(batch_responses) 
+        
+        # appending the model response
         selected_ds = selected_ds.add_column(name = "response", column = responses)
 
         if self_cons != False: #self_consistency responses
             selected_ds.save_to_disk(f'responses/{configs["model"]}/SC/{dataset}')
-        if CoT: # CoT responses
+
+        elif CoT: # CoT responses
             selected_ds.save_to_disk(f'responses/{configs["model"]}/CoT/{dataset}')
 
         else: # 0-shot, 1-shot, 3-shot reponses
