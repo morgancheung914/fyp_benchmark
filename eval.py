079f71096f5b3e5fc99eb29ae1ea2e62eebef039
wzqacky <wzqackyxd@gmail.com>
2025-02-16 16:11:04 +0800

    adding plot function for eval.py

--- eval.py
+++ eval.py
@@ -3,6 +3,7 @@ import json
 import yaml 
 import csv 
 import argparse
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="Argument parser for eval.py")
 parser.add_argument('-c', '--config', type=str, default=None, help='Path to the YAML config file')
@@ -20,7 +21,7 @@ else:
     dir = args.eval_path
 
 conf_mat = False 
-total_acc = []
+total_acc = {}
 for file in os.listdir(dir):
     if file[:8] == 'HaluEval': conf_mat, tp, fp, tn, fn = True, 0, 0, 0, 0
     else: correct = 0 
@@ -61,17 +62,34 @@ for file in os.listdir(dir):
                         fn += 1
 
     if file[:8] != 'HaluEval': 
-        total_acc.append(correct/len(data))
-        print(f"{file}: {total_acc[-1]}")
+        total_acc[file[:file.find('.')]] = correct/len(data)
+  
 
 # Report the average accuracy for medical QA datasets 
 if total_acc != []:
-    print(f"Average accuracy for medical QA datasets: {sum(total_acc)/len(total_acc)}")
+    model = dir[dir.find('/')+1:dir.find('/',dir.find('/')+1)]
+    print(f"Average accuracy for medical QA datasets: {sum(total_acc.values())/len(total_acc)}")
+
+    # Plotting the bar chart
+    d_name = list(total_acc.keys())
+    acc = list(total_acc.values())
+    colors = plt.cm.tab20.colors
+
+    plt.figure(figsize=(10, 5))
+    bars = plt.bar(d_name, acc, color=colors[:len(d_name)])
+    plt.xlabel('Dataset')
+    plt.ylabel('Accuracy')
+    plt.title(f'Accuracy for Medical QA Datasets by {model}')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    plt.legend(bars, d_name, title="Datasets")
+    plt.show()
 
 # Report the confusion matrix from the HaluEval dataset
 if conf_mat:
     print(f"""Confusion Matrix for HaluEval:
-       (predicted)  yes   no 
-(actual)       yes  {tp:<3} | {fn:<3}
-               no   {fp:<3} | {tn:<3}""")
+       yes   no 
+yes  {tp:<3} | {fn:<3}
+no   {fp:<3} | {tn:<3}""")
     print(f"Accuracy for HaluEval: {(tp+tn)/(tp+fn+fp+tn)}")
